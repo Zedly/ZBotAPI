39f61d798c3096c5f7caec13078f0de6192f70f1
Zedly <zedlydev@gmail.com>
2020-04-08 21:11:52 +0200

    Properly implemented the Ageable interface, allowing the detection of crop age

--- src/main/java/zedly/zbot/block/data/type/Fire.java
+++ src/main/java/zedly/zbot/block/data/type/Fire.java
@@ -1,10 +1,10 @@
 package zedly.zbot.block.data.type;
 
-import zedly.zbot.block.data.Ageable;
 import zedly.zbot.block.data.MultipleFacing;
+import zedly.zbot.block.data.AgeableBlock;
 
 /**
  * md_5's mixtape.
  */
-public interface Fire extends Ageable, MultipleFacing {
+public interface Fire extends AgeableBlock, MultipleFacing {
 }
